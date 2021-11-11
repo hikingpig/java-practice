9bf81107fd1e6d500de8cabdc3107d54769164f4
sehrfreud <phuocdongxoai@gmail.com>
2021-11-11 14:54:00 +0700

    [ADD] measurePerformance with BiFunction

--- helper/MeasurePerformance.java
+++ helper/MeasurePerformance.java
@@ -1,12 +1,21 @@
 package practice.helper;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class MeasurePerformance {
+  private static final int NANO_IN_MILI = 1_000_000;
   public static<T,R> R measurePerformance(Function<T, R> func, T input) {
     long start = System.nanoTime();
     R result = func.apply(input);
-    System.out.format("took: %dms%n", (System.nanoTime() - start)/1000000);
+    System.out.format("took: %dms%n", (System.nanoTime() - start)/NANO_IN_MILI);
+    return result;
+  }
+
+  public static<T,U,R> R measurePerformance(BiFunction<T,U,R> func, T arg1, U arg2) {
+    long start = System.nanoTime();
+    R result = func.apply(arg1, arg2);
+    System.out.format("took: %dms%n", (System.nanoTime() - start)/NANO_IN_MILI);
     return result;
   }
 }
