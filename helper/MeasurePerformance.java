f6b091fb38911b34589ad7cb97b82c7b038696b0
sehrfreud <phuocdongxoai@gmail.com>
2021-11-11 09:36:43 +0700

    [NEW] helper function to measure performace

---
+++ helper/MeasurePerformance.java
@@ -0,0 +1,12 @@
+package helper;
+
+import java.util.function.Function;
+
+public class MeasurePerformance {
+  public static<T,R> R measure(Function<T, R> func, T input) {
+    long start = System.nanoTime();
+    R result = func.apply(input);
+    System.out.format("took: %dms%n", (System.nanoTime() - start)/1000000);
+    return result;
+  }
+}
