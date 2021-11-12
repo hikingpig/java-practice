5350a355f4bdea47e12f76ed53108dd1061f152f
sehrfreud <phuocdongxoai@gmail.com>
2021-11-12 10:30:50 +0700

    [MOD] display time in microseconds

--- helper/MeasurePerformance.java
+++ helper/MeasurePerformance.java
@@ -7,6 +7,7 @@ import practice.helper.VargsFunction;
 
 public class MeasurePerformance {
   private static final int NANO_IN_MILI = 1_000_000;
+  private static final int NANO_IN_MICRO = 1_000;
   public static<T,R> R measurePerformance(Function<T, R> func, T input) {
     long start = System.nanoTime();
     R result = func.apply(input);
@@ -25,7 +26,7 @@ public class MeasurePerformance {
   public static <T,R> R measurePerformance(VargsFunction<T,R> func, T arg1, T... args) {
     long start = System.nanoTime();
     R result = func.apply(arg1, args);
-    System.out.format("took: %dms%n", (System.nanoTime() - start)/NANO_IN_MILI);
+    System.out.format("took: %dµs%n", (System.nanoTime() - start)/NANO_IN_MICRO);
     return result;
   }
 }
