8ceed14ea3eeef96702ac0fbf2446b7227c1c6ae
sehrfreud <phuocdongxoai@gmail.com>
2021-11-13 17:10:33 +0700

    [REV] refactor printing duration

--- helper/MeasurePerformance.java
+++ helper/MeasurePerformance.java
@@ -10,17 +10,21 @@ import practice.helper.VargsFunction;
 public class MeasurePerformance {
   private static final int NANO_IN_MILI = 1_000_000;
   private static final int NANO_IN_MICRO = 1_000;
+  private static void printDurationFrom(long start) {
+    long duration = System.nanoTime() - start;
+    System.out.format("🦄 took: %dms(%dµs)%n", duration/NANO_IN_MILI, duration/NANO_IN_MICRO);
+  }
   public static<T,R> R measurePerformance(Function<T, R> func, T input) {
     long start = System.nanoTime();
     R result = func.apply(input);
-    System.out.format("🦄 took: %dms%n", (System.nanoTime() - start)/NANO_IN_MILI);
+    printDurationFrom(start);
     return result;
   }
 
   public static<T,U,R> R measurePerformance(BiFunction<T,U,R> func, T arg1, U arg2) {
     long start = System.nanoTime();
     R result = func.apply(arg1, arg2);
-    System.out.format("🦄 took: %dms%n", (System.nanoTime() - start)/NANO_IN_MILI);
+    printDurationFrom(start);
     return result;
   }
 
@@ -28,15 +32,13 @@ public class MeasurePerformance {
   public static <T, U, V> V measurePerformance(VargsFunction<T, U, V> func, T arg1, U... args) {
     long start = System.nanoTime();
     V result = func.apply(arg1, args);
-    System.out.format("🦄 took: %dµs%n", (System.nanoTime() - start)/NANO_IN_MICRO);
+    printDurationFrom(start);
     return result;
   }
 
   public static <T> void measurePerformance(Consumer<T> func, T arg) {
     long start = System.nanoTime();
     func.accept(arg);
-    System.out.format("🦄 took: %dms%n", (System.nanoTime() - start)/NANO_IN_MILI);
+    printDurationFrom(start);
   }
-
-
 }
